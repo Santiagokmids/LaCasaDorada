2e50a2b165ef370b65ef6d8053bd0d8b83f8b06d
Santiago <sntgtrochez@gmail.com>
2021-03-14 15:03:07 -0500

    With orders

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,93 @@
 package model;
 
-public enum Order {
-	REQUESTED,IN_PROCESS,SENT,DELIVERED;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Order {
+	
+	//Attributes
+	private String code;
+	private StateOrder state;
+	private ArrayList<Integer> amount;
+	private Date date;
+	private String fieldOfObservations;
+	
+	//Relations
+	private Client orderClient;
+	private ArrayList<Product> products;
+	private Employee orderEmployee;
+	
+	public Order(String code, StateOrder state, ArrayList<Integer> amount, Date date, String fieldOfObservations) {
+		this.code = code;
+		this.state = state;
+		products = new ArrayList<>();
+		this.amount = amount;
+		this.date = date;
+		this.fieldOfObservations = fieldOfObservations;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public StateOrder getState() {
+		return state;
+	}
+
+	public void setState(StateOrder state) {
+		this.state = state;
+	}
+
+	public ArrayList<Integer> getAmount() {
+		return amount;
+	}
+
+	public void setAmount(ArrayList<Integer> amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getFieldOfObservations() {
+		return fieldOfObservations;
+	}
+
+	public void setFieldOfObservations(String fieldOfObservations) {
+		this.fieldOfObservations = fieldOfObservations;
+	}
+
+	public Employee getOrderEmployee() {
+		return orderEmployee;
+	}
+
+	public void setOrderEmployee(Employee orderEmployee) {
+		this.orderEmployee = orderEmployee;
+	}
+
+	public Client getOrderClient() {
+		return orderClient;
+	}
+
+	public void setOrderClient(Client orderClient) {
+		this.orderClient = orderClient;
+	}
+
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+	
 }
---
+++ src/model/StateOrder.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum StateOrder {
+	REQUESTED,IN_PROCESS,SENT,DELIVERED;
+}
